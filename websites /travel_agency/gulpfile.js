812d166f3aecc8dccdbb887fd8f1ea404dc0acc7
deedeeh <dinahafez.8@gmail.com>
2019-09-08 23:49:22 +0100

    Travel agency website with html and  postcss files done with Gulp

---
+++ websites /travel_agency/gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+
+gulp.task('default', () => {
+  console.log('Created a Gulp task!');
+});
+
+gulp.task('html', () => {
+  console.log('Here I can do things to my HTML.');
+});
+
+gulp.task('styles', () => {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([nested, cssvars, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', () => {
+
+  watch('./app/index.html', () => {
+    gulp.start('html');
+  })
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('styles');
+  });
+
+});
+
+
+
+
+
+
+
