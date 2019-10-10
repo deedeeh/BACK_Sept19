b09bb8096c48b98c4fbe2c71522bd863a2ddb9be
deedeeh <dinahafez.8@gmail.com>
2019-10-10 22:27:09 +0100

    Added a new gulp task to update sprite file and delete old one

--- websites /travel_agency/gulp/tasks/sprites.js
+++ websites /travel_agency/gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const svgSprite = require('gulp-svg-sprite');
 const rename = require('gulp-rename');
+const del = require('del');
 
 const config = {
   mode: {
@@ -15,7 +16,11 @@ const config = {
   }
 }
 
-gulp.task('createSprite', () => {
+gulp.task('beginClean', () => {
+  return del(['./app/temp/sprite', './app/assets/images/sprites']);
+})
+
+gulp.task('createSprite', ['beginClean'], () => {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite'))
@@ -32,4 +37,4 @@ gulp.task('copySpriteCSS', ['createSprite'], () => {
     .pipe(gulp.dest('./app/assets/styles/modules'));
 })
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
